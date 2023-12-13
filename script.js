1da3d6296e88382ef3ff7d92d057557775d76cd9
natnael amare <natnaelamare706@gmail.com>
2023-12-13 10:09:08 +0300

    finalizing qoute

--- script.js
+++ script.js
@@ -12,13 +12,41 @@ const qouteText = document.getElementById('qoute');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQouteBtn = document.getElementById('new-qoute');
+const loader = document.getElementById('loader');
+
 let apiQuotes = [];
+
+function loading (){
+    loader.hidden = false;
+    qouteContainer.hidden = true;
+}
+function complete (){
+    loader.hidden = true;
+    qouteContainer.hidden = false;
+}
+
 function newQoute () {
+    loading();
  const qoute = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+ if(!qoute.author){
+    authorText.textContent = 'Unknown';
+ }else{
+    authorText.textContent = qoute.author;
+ }
+
+ if(qoute.text.length > 50){
+    qouteText.classList.add('long-qoute');
+ }else{
+    qouteText.classList.remove('long-qoute');
+ }
+ qouteText.textContent = qoute.text;
+ complete();
+
  authorText.textContent = qoute.author;
  qouteText.textContent = qoute.text;
 }
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
@@ -28,6 +56,13 @@ async function getQuotes() {
         console.log(error);
     }
 }
+function tweetQoute(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${qouteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+newQouteBtn.addEventListener('click', newQoute);
+twitterBtn.addEventListener('click', tweetQoute)
 getQuotes();
 
 
